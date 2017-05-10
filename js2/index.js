066359804e464de2670c4fba19d1d1117e7df3b8
zhougogo <1290612478@qq.com>
2017-05-10 13:36:46 +0800

    shumeng
    数梦- pc端

---
+++ js2/index.js
@@ -0,0 +1,24 @@
+//	bodySize();
+	function bodySize(){
+		var WinWidth = window.screen.width;
+		console.log(WinWidth);
+		document.body.style.width = WinWidth + "px";
+		// var WinHeight = window.innerHeight;
+		// var bodyWidth = document.defaultView.getComputedStyle(document.body,null).width;
+		// if(parseFloat(bodyHeight) < WinHeight){
+		// 	document.body.style.width = bodyWidth + "px";
+		// }
+	}
+
+
+//$('.all').css('width',clientWidth*8/10);
+//console.log(123);
+
+//$('.popup img').eq(1).click(function(){
+//	
+//	$('.popup').css('display','none');
+//	
+////	console.log(123);
+//	
+//})
+
